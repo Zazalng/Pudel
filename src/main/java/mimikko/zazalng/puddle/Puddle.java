e3e4cbe3683b0d50c65c5fd63d317e34a23049a4
Zazalng Napapon <kenghide@hotmail.com>
2023-06-20 00:06:16 +0700

    Remove api key

--- src/main/java/mimikko/zazalng/puddle/Puddle.java
+++ src/main/java/mimikko/zazalng/puddle/Puddle.java
@@ -17,7 +17,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Puddle{
 
     public static void main(String[] args) throws Exception {
-        String botToken = "OTA4NzI0NDAwNzM4NjcyNzEw.Gp9yft.2HwqZ0NZ5-P5F-wUI9gsDXq97npqQjEpnn7goE";
+        String botToken = "";
 
         JDA jda = JDABuilder.createLight(botToken, 
                 GatewayIntent.DIRECT_MESSAGES, 
