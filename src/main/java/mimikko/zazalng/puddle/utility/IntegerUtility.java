b8d40cd07a844ae5ab974e4af3f0854ba12e240e
Zazalng Napapon <kenghide@hotmail.com>
2024-08-23 04:09:38 +0700

    Commit Daily Home mini fix
    Knowing Bugs:
    - Stopping World

--- src/main/java/mimikko/zazalng/puddle/utility/IntegerUtility.java
+++ src/main/java/mimikko/zazalng/puddle/utility/IntegerUtility.java
@@ -2,10 +2,10 @@ package mimikko.zazalng.puddle.utility;
 
 public class IntegerUtility {
     public static int randomInt(int max){
-        return (int) (Math.random()*(max+1));
+        return (int) (Math.random()*(max));
     }
 
     public static int randomInt(int min, int max){
-        return (int) (Math.random()*(max - min + 1) + min);
+        return (int) (Math.random()*(max - min) + min);
     }
 }
