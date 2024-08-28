cda65e84790318c5b4f6e04af23035defb7e5399
Zazalng Napapon <kenghide@hotmail.com>
2024-08-28 20:10:36 +0700

    Commit hotfix - Last update for master branch (To implement continuely Command)
    known bug:
    - Stopping World

--- src/main/java/mimikko/zazalng/puddle/handlers/EventHandler.java
+++ src/main/java/mimikko/zazalng/puddle/handlers/EventHandler.java
@@ -49,7 +49,7 @@ public class EventHandler extends ListenerAdapter{
             String fullRespond = "From "+e.getGuild().getName()+" in "+e.getGuildChannel().getName()+" by "+e.getAuthor().getName()+" said: \n"+e.getMessage().getContentRaw();
             System.out.println(fullRespond);
 
-            e.getChannel().sendTyping().queue();
+            //e.getChannel().sendTyping().queue();
             new CommandHandler(getGuildEntity(e.getGuild()), getUserEntity(e.getAuthor()), e);
         }
     }
