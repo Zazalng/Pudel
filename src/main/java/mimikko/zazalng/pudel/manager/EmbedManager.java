f20b2f9158c1afa286d62e656972a2a6d79759b9
Zazalng Napapon <kenghide@hotmail.com>
2024-10-01 19:03:16 +0700

    Minor commit - Update Library

--- src/main/java/mimikko/zazalng/pudel/manager/EmbedManager.java
+++ src/main/java/mimikko/zazalng/pudel/manager/EmbedManager.java
@@ -1,17 +1,50 @@
 package mimikko.zazalng.pudel.manager;
 
 import mimikko.zazalng.pudel.PudelWorld;
+import mimikko.zazalng.pudel.entities.SessionEntity;
 import net.dv8tion.jda.api.EmbedBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class EmbedManager implements Manager{
-    protected final PudelWorld pudelWorld;
+import java.awt.*;
 
-    public EmbedManager(PudelWorld pudelWorld){
+public class EmbedManager implements Manager {
+
+    private final PudelWorld pudelWorld; // Your bot world
+    private final Logger logger = LoggerFactory.getLogger(EmbedManager.class);
+
+    public EmbedManager(PudelWorld pudelWorld) {
         this.pudelWorld = pudelWorld;
     }
 
-    public EmbedBuilder getBuilder(){
-        return new EmbedBuilder();
+    // Create a reusable template for an embed
+    public EmbedBuilder createBasicEmbed() {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setColor(Color.BLUE);  // Set a default color
+        builder.setFooter("Powered by PudelBot");  // Set a default footer
+        return builder;
+    }
+
+    // Example method to create a help embed
+    public EmbedBuilder createHelpEmbed(String title, String description) {
+        EmbedBuilder builder = createBasicEmbed();
+        builder.setTitle(title);
+        builder.setDescription(description);
+        return builder;
+    }
+
+    // Example method to create an error embed
+    public EmbedBuilder createErrorEmbed(String errorMessage) {
+        EmbedBuilder builder = createBasicEmbed();
+        builder.setColor(Color.RED);  // Change color for errors
+        builder.setTitle("Error");
+        builder.setDescription(errorMessage);
+        return builder;
+    }
+
+    // Process a finished embed
+    public void sendEmbed(SessionEntity session, EmbedBuilder embedBuilder) {
+        session.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
     }
 
     @Override
@@ -21,16 +54,16 @@ public class EmbedManager implements Manager{
 
     @Override
     public void initialize() {
-
+        logger.info("EmbedManager initialized.");
     }
 
     @Override
     public void reload() {
-
+        logger.info("EmbedManager reloaded.");
     }
 
     @Override
     public void shutdown() {
-
+        logger.info("EmbedManager shut down.");
     }
 }
